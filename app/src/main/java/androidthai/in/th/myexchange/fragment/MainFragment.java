f600baa43c3e130a46f22636f616f652d5e1faf9
MasterUNG <phrombutr@gmail.com>
2018-01-06 16:02:25 +0700

    Finish 6Jan

---
+++ app/src/main/java/androidthai/in/th/myexchange/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package androidthai.in.th.myexchange.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidthai.in.th.myexchange.R;
+
+/**
+ * Created by masterung on 6/1/2018 AD.
+ */
+
+public class MainFragment extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
