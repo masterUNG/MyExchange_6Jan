e181d5b69ec815ef189c748e07e90c93bfafedec
MasterUNG <phrombutr@gmail.com>
2018-01-07 11:39:15 +0700

    Exchange Money Finish

--- app/src/main/java/androidthai/in/th/myexchange/utility/MyGetRateFromAPI.java
+++ app/src/main/java/androidthai/in/th/myexchange/utility/MyGetRateFromAPI.java
@@ -1,8 +1,18 @@
 package androidthai.in.th.myexchange.utility;
 
+import android.content.Context;
+
 /**
  * Created by masterung on 7/1/2018 AD.
  */
 
 public class MyGetRateFromAPI {
-}
+
+    private Context context;
+
+    public MyGetRateFromAPI(Context context) {
+        this.context = context;
+    }
+
+
+}   // Main Class
